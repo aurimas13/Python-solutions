62fe8f7d88de41dda39572fd3378db14dd8ebe0d
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-03 09:17:39 +0200

    Update Exercise_7.py

--- Exercise_7.py
+++ Exercise_7.py
@@ -1,4 +1,4 @@
-print "Mary had a little lamb."
+print "Mary had a little lamb." 
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
 print "." * 10 # what'd that do?
