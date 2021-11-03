de8f8b6680280087d981498936eec489a1431738
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-03 09:25:03 +0200

    Update Exercise_12.py

--- Exercise_12.py
+++ Exercise_12.py
@@ -1,3 +1,7 @@
+# Exercise_12.py
+#
+# Updated by Aurimas A. Nausedas on 11/03/21.
+
 age = raw_input("How old are you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
