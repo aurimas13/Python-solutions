1f78962ec89ebc5d8709c50c96b39479f76e139c
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-03 09:17:26 +0200

    Update Exercise_6.py

--- Exercise_6.py
+++ Exercise_6.py
@@ -1,4 +1,4 @@
-x = "There are %d types of people." % 10
+x = "There are %d types of people." % 10  
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
