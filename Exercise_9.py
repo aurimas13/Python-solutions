b5312e6a18a87c5e643764976136b944cb6dc633
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-03 09:18:10 +0200

    Update Exercise_9.py

--- Exercise_9.py
+++ Exercise_9.py
@@ -1,4 +1,4 @@
-# Here's some new strange stuff, remember type it exactly.
+# Here's some new strange stuff, remember type it exactly. 
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
