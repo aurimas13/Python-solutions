a49e3d10fe4b71eafbaae840b9e21523843b6098
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-03 09:16:50 +0200

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -5,7 +5,7 @@ passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car 
-average_passengers_per_car = passengers / cars_driven
+average_passengers_per_car = passengers / cars_driven 
 
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
