9fdab343a855c7bcbe630b83a8f1abad0f26bc35
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-08 09:52:45 +0200

    Update Exercise_9.py

--- Exercise_9.py
+++ Exercise_9.py
@@ -1,7 +1,8 @@
 # Solution of Exercise 9 - Exercise_9.py
 #
+# Uploaded by Aurimas A. Nausedas on 11/23/20.
 # Updated by Aurimas A. Nausedas on 11/06/21.
-# 
+#
 # Here's some new strange stuff, remember type it exactly. 
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
