da3da401f35844f855e129bd8181df1e12a4df1e
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-08 09:49:42 +0200

    Update Exercise_39.py

--- Exercise_39.py
+++ Exercise_39.py
@@ -1,5 +1,6 @@
 # Solution of Exercise 39 - Exercise_39.py
 #
+# Uploaded by Aurimas A. Nausedas on 11/23/20.
 # Updated by Aurimas A. Nausedas on 11/06/21.
 
 # create a mapping of state to abbreviation
