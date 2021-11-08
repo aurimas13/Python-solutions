77df25ccad2a208a9b7b303efbe90804328cf3f4
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-08 09:48:40 +0200

    Update Exercise_31.py

--- Exercise_31.py
+++ Exercise_31.py
@@ -1,5 +1,6 @@
 # Solution of Exercise 31 - Exercise_31.py
 #
+# Uploaded by Aurimas A. Nausedas on 11/23/20.
 # Updated by Aurimas A. Nausedas on 11/06/21.
 
 print "You enter a dark room with two doors. Do you go through door #1 or door #2?" 
